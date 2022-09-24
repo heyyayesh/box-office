367049fee10bf36893db5d47008f53ea6289ed51
Ayesh Biswal <biswalayesh3@gmail.com>
2022-09-24 21:24:26 +0530

    Add MovieDetails component

---
+++ client/src/components/MovieDetails.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const MovieDetails = () => {
+  const { id } = useParams();
+
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const response = await fetch(`http://localhost:5000/movies/${id}`);
+      const data = await response.json();
+      setMovie(data);
+      console.log(data);
+    };
+
+    getMovie();
+  }, []);
+
+  return movie ? (
+    <div>
+      <h1>{movie.title}</h1>
+      <p>{movie.description}</p>
+    </div>
+  ) : null;
+};
+
+export default MovieDetails;
